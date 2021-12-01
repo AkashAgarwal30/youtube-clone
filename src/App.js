68b72ab12550521129d473a9ce58fe04300f0bce
Akashagarwal <akashag30j2000@gmail.com>
2021-12-01 20:01:05 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {youtubeLibraryLoaded} from './store/actions/api';
 import Trending from './containers/Trending/Trending';
 import Search from './containers/Search/Search';
 
-const API_KEY = process.env.AIzaSyDV59At-BhS1RRyXLcdo85crpMhoZAcnqM;
+const API_KEY = "AIzaSyDV59At-BhS1RRyXLcdo85crpMhoZAcnqM";
 
 class App extends Component {
   render() {
