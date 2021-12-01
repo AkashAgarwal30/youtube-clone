3120c9d1483a87f6e492b5d3f30ef51e438f9372
Akashagarwal <akashag30j2000@gmail.com>
2021-12-01 20:16:23 +0530

    Updates

---
+++ precache-manifest.80f20f4e02922a8bf71e3dbd0e223468.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7814743d5b63f5b84dbdd7848e8468d5",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "81238997ab5745ecaee3",
+    "url": "/youtube-clone/static/css/2.8b0d8636.chunk.css"
+  },
+  {
+    "revision": "5f8e50d7214d03ac72ea",
+    "url": "/youtube-clone/static/css/main.80bd069b.chunk.css"
+  },
+  {
+    "revision": "81238997ab5745ecaee3",
+    "url": "/youtube-clone/static/js/2.6d92cd13.chunk.js"
+  },
+  {
+    "revision": "9e758a74e9ea38dcbd7741eb3eff2172",
+    "url": "/youtube-clone/static/js/2.6d92cd13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f8e50d7214d03ac72ea",
+    "url": "/youtube-clone/static/js/main.4524a8c1.chunk.js"
+  },
+  {
+    "revision": "8be09c3ba817013d293c",
+    "url": "/youtube-clone/static/js/runtime-main.432884e3.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/youtube-clone/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/youtube-clone/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/youtube-clone/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/youtube-clone/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/youtube-clone/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/youtube-clone/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/youtube-clone/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/youtube-clone/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/youtube-clone/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/youtube-clone/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/youtube-clone/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e5520ae279d2f04880c75932b3e60ded",
+    "url": "/youtube-clone/static/media/logo.e5520ae2.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/youtube-clone/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/youtube-clone/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/youtube-clone/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/youtube-clone/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/youtube-clone/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
